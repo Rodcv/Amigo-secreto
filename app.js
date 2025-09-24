656f01389d1e3bcd72f676414c586ab81ea17913
Rodcv <jorgemrodcv@gmail.com>
2025-09-23 20:01:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  nombre = document.getElementById("agregarAmigo")
+  if (nombre == "") {
+    alert("Por favor, inserte un nombre.");
+    return;}
+  amigos.push(nombre);
+  nombre.value = "";
